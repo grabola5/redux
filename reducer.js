4be93966f7a33bb0e466a845cc585e6f0f191f6f
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-09-20 15:12:37 +0200

    Add reducers

---
+++ reducer.js
@@ -0,0 +1,62 @@
+import {ADD_COMMENT} from '/actions';
+import {EDIT_COMMENT} from '/actions';
+import {REMOVE_COMMENT} from '/actions';
+import {THUMB_UP_COMMENT} from '/actions';
+import {THUMB_DOWN_COMMENT} from '/actions'
+
+
+const initialState = {
+  comments: [],
+  users: []
+};
+
+function reducer(state=initialState, action) {
+  switch(action.type) {
+    case ADD_COMMENT:
+      return Object.assign({}, state, {
+        comments: [
+          {
+            id: action.id,
+            text: action.text,
+            votes: 0
+          }
+          , ...state]
+      });
+    case REMOVE_COMMENT:
+      return Object.assign({}, state, {
+        comments: state.comments.filter( comment => comment.id !== action.id)
+      });
+    case EDIT_COMMENT:
+      return Object.assign({}, state, {
+        comments: [
+          {
+            id: action.id,
+            text: action.text,
+            votes: action.votes
+          }
+          , ...state]
+      });
+    case THUMB_UP_COMMENT:
+      return Object.assign({}, state, {
+        comments: [
+          {
+            id: action.id,
+            text: action.text,
+            votes: action.votes +1
+          }
+          , ...state]
+      });
+    case THUMB_DOWN_COMMENT:
+      return Object.assign({}. state, {
+        comments: [
+          {
+            id: action.id,
+            text: action.text,
+            votes: action.votes -1
+          }
+          , ...state]
+      });
+    default:
+      return state;
+  }
+}
