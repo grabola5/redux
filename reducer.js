8e6def2a6825d5f86df69dc26bb6cf1128803fcb
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-09-20 20:44:00 +0200

    Create reducer comments

--- reducer.js
+++ reducer.js
@@ -1,8 +1,6 @@
-import {ADD_COMMENT} from '/actions';
-import {EDIT_COMMENT} from '/actions';
-import {REMOVE_COMMENT} from '/actions';
-import {THUMB_UP_COMMENT} from '/actions';
-import {THUMB_DOWN_COMMENT} from '/actions'
+import { combineReducers } from 'redux';
+import comments from './comments';
+import users from './users';
 
 
 const initialState = {
@@ -10,53 +8,7 @@ const initialState = {
   users: []
 };
 
-function reducer(state=initialState, action) {
-  switch(action.type) {
-    case ADD_COMMENT:
-      return Object.assign({}, state, {
-        comments: [
-          {
-            id: action.id,
-            text: action.text,
-            votes: 0
-          }
-          , ...state]
-      });
-    case REMOVE_COMMENT:
-      return Object.assign({}, state, {
-        comments: state.comments.filter( comment => comment.id !== action.id)
-      });
-    case EDIT_COMMENT:
-      return Object.assign({}, state, {
-        comments: [
-          {
-            id: action.id,
-            text: action.text,
-            votes: action.votes
-          }
-          , ...state]
-      });
-    case THUMB_UP_COMMENT:
-      return Object.assign({}, state, {
-        comments: [
-          {
-            id: action.id,
-            text: action.text,
-            votes: action.votes +1
-          }
-          , ...state]
-      });
-    case THUMB_DOWN_COMMENT:
-      return Object.assign({}. state, {
-        comments: [
-          {
-            id: action.id,
-            text: action.text,
-            votes: action.votes -1
-          }
-          , ...state]
-      });
-    default:
-      return state;
-  }
-}
+const app = combineReducers({
+  comments,
+  users
+})
